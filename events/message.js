0fc1543166a4ddbd0e44d25a598ae12a972dd9ca
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-11-21 10:56:34 +0000

    Basic command handler

---
+++ events/message.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+    register(client, msg, db){
+        if (msg.author.bot){return}
+        let message = String(msg.content).toLowerCase()
+        let guild = msg.guild
+        db.get('SELECT "value" FROM "' + guild + '" WHERE key="prefix"', (err, row) => {
+            if (err){
+                console.log(err)
+            }
+            let prefix = String(row.value)
+            const args = msg.content.slice(prefix.length).trim().split(/ +/);
+            const command = args.shift().toLowerCase();
+            if (message.startsWith(prefix)){
+                if (!client.commands.has(command)) return;
+                try {
+                    client.commands.get(command).execute(msg, args);
+                } catch (error) {
+                    console.error(error);
+                    msg.reply('there was an error trying to execute that command!');
+                }
+            }
+        })
+        
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // Mr imports
 const Discord = require('discord.js');
+const fs = require('fs');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 const config = require('./config.json')
 const sqlite3 = require('sqlite3').verbose();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 // Events
 const guildCreate = require('./events/guildCreate')
 const guildDelete = require('./events/guildDelete')
+const message = require('./events/message')
 
-let db = new sqlite3.Database('database.db', (err) => {
+let db = new sqlite3.cached.Database('database.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
@@ -18,6 +29,6 @@ client.on('ready', () => {
    console.log('Logged in');
 });
 client.on("guildCreate", guild => {guildCreate.register(guild, db)})
-client.on("guildDelete", guild => { guildDelete.register(guild, db)})
-
+client.on("guildDelete", guild  => { guildDelete.register(guild, db)})
+client.on("message", msg => { message.register(client, msg, db) })
 client.login(config.token);
