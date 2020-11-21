dddb0bea8382dffdffef69c921f9f5b39d93c1c6
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-11-21 12:20:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
 const config = require('./config.json')
 const sqlite3 = require('sqlite3').verbose();
+
+// Registers all the commands in the commands folder
+client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
 }
 
