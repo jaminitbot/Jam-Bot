f551753b1a54a76e72e67854fc025011aa2275bc
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-11-21 11:58:16 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,11 +2,11 @@ module.exports = {
 	name: 'ban',
 	description: 'Bans a user',
 	execute(client, message, args, db) {
-        if(!message.member.hasPermission(["BAN_MEMBERS"])) return message.reply("You do not have permission to perform this command!");
-        if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command, try inviting me again!");
-        const memberToBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if(!message.member.hasPermission(["BAN_MEMBERS"])) return message.reply("You do not have permission to perform this command!") // Checks if the user can ban
+        if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command, try inviting me again!") // Checks if bot can ban
+        const memberToBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]); // Gets the mentioned user
         if (!memberToBan) return message.reply('You didn\'t mention a valid user')
-        if (message.author.id == memberToBan.id) return message.reply('You can\'t ban yourself!')
+        if (message.author.id == memberToBan.id) return message.reply('You can\'t ban yourself!') // Checks if the user mentioned themself
         let reason = args.splice(1).join(' ');
         const authorRole = message.member.roles.highest;
         const targetRole = memberToBan.roles.highest;
