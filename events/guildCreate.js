9f1daad6510b9c79b68de80ed8de665dddf6f641
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-11-21 12:20:28 +0000

    Add default prefix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,7 +4,12 @@ module.exports = {
         db.run('CREATE TABLE "' + guild.id + '" (key varchar(255), value varchar(255))', (err) => {
         if (err) {
             return console.error(err.message);
-        }   
-    })
+        }
+        db.run('INSERT INTO "' + guild.id + '" (key, value) VALUES (\'prefix\', \'!\')', (err) => {
+            if (err) {
+                return console.error(err.message);
+            }   
+        })
+        })
     }
 }
