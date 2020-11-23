e56fa4e6529a69082e08935efb1fcb9d7d48520f
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-11-23 16:58:05 +0000

    remove verbose messages

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,5 @@
 module.exports = {
     register(guild, db, config){
-        console.log("Joined a new guild: " + guild.name);
         db.run('CREATE TABLE "' + guild.id + '" (key varchar(255), value varchar(255))', (err) => {
         if (err) {
             return console.error(err.message);
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,5 @@
 module.exports = {
     register(guild, db){
-        console.log("Left a guild: " + guild.name);
         db.run('DROP TABLE "' + guild.id + '"', (err) => {
         if (err) {
             return console.error(err.message);
