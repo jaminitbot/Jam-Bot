1437a6f2a37b2efe4cd7f203001dce2734a230f9
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-11-23 16:44:28 +0000

    Add tool to add guilds to database if bot was online
    TODO: #3

---
+++ tools/fixDatabase.js
@@ -0,0 +1,17 @@
+// Runs the things in guildCreate incase the bot is not online when the guild is joined
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('../config.json')
+const guildCreate = require('../events/guildCreate')
+const sqlite3 = require('sqlite3').verbose()
+let db = new sqlite3.cached.Database("../" + config.settings.databaseLocation, (err) => {
+    if (err) return console.error(err.message)
+    console.log('Connected to the SQlite database')
+  });
+
+client.on("ready", () => {
+    client.guilds.cache.forEach((guild) => {
+        guildCreate.register(guild, db, config)
+})
+})
+client.login(config.discord.token)
