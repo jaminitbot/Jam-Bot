283cf07f0c9e3949b238e2e026cde3b9fe73d343
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-12-23 20:00:53 +0000

    Owner

---
+++ commands/owner.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'owner',
+	description: 'Displays the owner of the bot',
+	permissions: '',
+	usage: '!owner',
+	execute(client, message, args, db) {
+        message.reply(`The owner of the bot is: ${config.settings.owner}`)
+	},
+};
