59e780ed5aa82df95e4cf9f83f993fd461b83a58
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2020-12-24 15:39:37 +0000

    Dynamic help text?

--- commands/help.js
+++ commands/help.js
@@ -7,6 +7,14 @@ module.exports = {
 	description: 'Displays a list of avaliable commands',
 	usage: 'help command',
 	execute(client, message, args, db) {
+		db.get('SELECT "value" FROM "' + message.guild + '" WHERE key="prefix"', (err, row) => { // Get prefix
+			if (err) console.log(err)
+			if (row){
+				prefix = String(row.value)
+			} else {
+				prefix = config.defaults.prefix
+			}
+		})
 		if (args[0]){
 			const commandToFind = String(args[0]).toLowerCase()
 			if (commandToFind && !(commandToFind == ' ')){
@@ -17,14 +25,6 @@ module.exports = {
 				}
 				const usage = command.usage
 				if (!usage) return message.channel.send('Command doesn\'t have a usage yet!')
-				db.get('SELECT "value" FROM "' + message.guild + '" WHERE key="prefix"', (err, row) => { // Get prefix
-					if (err) console.log(err)
-					if (row){
-						prefix = String(row.value)
-					} else {
-						prefix = config.defaults.prefix
-					}
-				})
 				const permissionsNeeded = command.permissions || 'None'
 				let embed = {
 					'title': prefix + commandToFind,
@@ -37,7 +37,7 @@ module.exports = {
 		const commandToFind = String(args[0]).toLowerCase()
 		let embed = {
 			'title': 'Help',
-			'description': 'You can view a list of commands [here](https://jam-bot-discord.github.io/Jam-Bot/)',
+			'description': `You can view a list of commands [here](https://jam-bot-discord.github.io/Jam-Bot/?prefix=${prefix})`,
 			'color': '0eacc4'
 		}
 		message.channel.send({embed: embed})
