8bd079d4f2653e2908a03bf21fcfc24c396858c8
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-07 21:42:50 +0000

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,14 +1,13 @@
 const config = require('../config.json')
 module.exports = {
-
 	name: 'shutdown',
 	description: 'STOPS THE BOT',
 	usage: '!shutdown',
 	execute(client, message, args, db) {
 		if (config.settings.ownerid == message.author.id){
-			process.exit()
+			return process.exit()
 		} else {
-			message.channel.send('Nice try.')
+			return message.channel.send('Nice try.')
 		}
         
 	},
