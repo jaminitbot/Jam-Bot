563ae0b97173fe95c591e40f1b6b7527d6c0dcf8
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-08 13:29:00 +0000

    better var names

--- functions/kickorban.js
+++ functions/kickorban.js
@@ -1,18 +1,18 @@
 module.exports = {
-    execute(message, args, type){
+    execute(message, args, banOrMute){
         if (!args[0]) return message.reply('Usage: ' + this.usage)
-		if(!message.guild.me.hasPermission(['BAN_MEMBERS', 'ADMINISTRATOR', 'KICK_MEMBERS'])) return message.channel.send(`I don't have permission to perform this command, check I can ${type} people!`)
+		if(!message.guild.me.hasPermission(['BAN_MEMBERS', 'ADMINISTRATOR', 'KICK_MEMBERS'])) return message.channel.send(`I don't have permission to perform this command, check I can ${banOrMute} people!`)
 		const memberToBan = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-		if (!memberToBan) return message.reply('You didn\'t mention a valid user in this guild!') // Prolly should be more descriptive to user
-		if (message.author.id == memberToBan.id) return message.reply(`You can't ${type} yourself silly!`)
-		const userName = message.author.username + "#" + message.author.discriminator
+		if (!memberToBan) return message.reply('You didn\'t mention a valid user in this server!')
+		if (message.author.id == memberToBan.id) return message.reply(`You can't ${banOrMute} yourself silly!`)
+		const moderator = message.author.username + "#" + message.author.discriminator
 		let reason = args.splice(1).join(' ')
-        if (type == 'ban'){
-            memberToBan.ban({reason: `${userName}: ${reason}`, days: 1})
+        if (banOrMute == 'ban'){
+            memberToBan.ban(`${moderator}: ${reason}`, 1)
         } else {
-            memberToBan.kick({reason: `${userName}: ${reason}`})
+            memberToBan.kick(`${moderator}: ${reason}`)
         }
 		
-		message.reply(`${memberToBan} was ${type}ed with reason: ${reason}`)
+		message.reply(`${memberToBan} was ${banOrMute}ed with reason: ${reason}`)
     }
 }
