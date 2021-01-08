75377fcac631f8fc07c0e01ca5a0e5786591a4b2
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-08 11:42:30 +0000

    Create jack.js

---
+++ commands/jack.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'jack',
+	description: 'Jack',
+	usage: 'jack',
+	execute(client, message, args, db) {
+        message.channel.send('<@523963702245064725>')
+        message.channel.send('I hate him')
+	},
+};
