5c3f566243f2350ef9936b239b0c53073478ee56
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-08 20:44:34 +0000

    yes

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'Latency!',
 	usage: 'ping',
 	execute(client, message, args, db) {
-		message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`) // https://stackoverflow.com/a/63411714
+		message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms🏓`) // https://stackoverflow.com/a/63411714
 	},
 }
