bb4e9dec38fbfb418fd1cfa4432f41703053b7bb
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-09 16:56:44 +0000

    Yes image

--- commands/image.js
+++ commands/image.js
@@ -1,14 +1,23 @@
-const gis = require('g-i-s');
+const gis = require('g-i-s')
+const isImage = require('is-image')
 module.exports = {
 	name: 'image',
 	description: 'Gets a image',
 	usage: 'image duck',
-	execute(client, message, args, db) {
+	async execute(client, message, args, db) {
         if (!args[0]) return message.channel.send('You need to search for something')
-        let search = args.splice(1).join(' ')
+        let search = args.splice(0).join(' ')
+        let done = false
         gis(search, function(error, results){
             if (error) return
-            message.channel.send(results[0].url)
+            results.forEach(element => {
+                if (isImage(element.url)){
+                    if (!done){
+                        done = true
+                        return message.channel.send(element.url)
+                    }
+                }
+            })
         })
 	},
 };
