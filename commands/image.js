11e0742163091dc03c06615d4cb2792ada340941
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-09 16:28:56 +0000

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -5,7 +5,8 @@ module.exports = {
 	usage: 'image duck',
 	execute(client, message, args, db) {
         if (!args[0]) return message.channel.send('You need to search for something')
-        gis(args[0], function(error, results){
+        let search = args.splice(1).join(' ')
+        gis(search, function(error, results){
             if (error) return
             message.channel.send(results[0].url)
         })
