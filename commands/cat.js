ebb75806320fd1bc0ee977e5509703b766a5d46e
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-09 15:28:22 +0000

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'cat',
 	description: 'Purrrr',
 	usage: 'cat',
-	execute(client, message, args, db) {
+	async execute(client, message, args, db) {
         const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
         message.channel.send(file);
 	},
