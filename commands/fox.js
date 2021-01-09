9a994a801ec1000d55974a63dd53532c0992b2e2
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-09 15:33:46 +0000

    Create fox.js

---
+++ commands/fox.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch')
+module.exports = {
+	name: 'fox',
+	description: 'FOX',
+	usage: 'fox',
+	async execute(client, message, args, db) {
+        const { image } = await fetch('https://randomfox.ca/floof/').then(response => response.json());
+        message.channel.send(image);
+	},
+};
