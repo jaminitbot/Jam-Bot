28b8d6f6f6da482c10d472941323b8093e694c73
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-09 15:31:47 +0000

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'Woof',
 	usage: 'dog',
 	async execute(client, message, args, db) {
-        const { message } = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
-        message.channel.send(file);
+        const data = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
+        message.channel.send(data.message);
 	},
 };
