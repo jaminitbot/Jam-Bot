497168aac187100eb37724632fb914ccc076fd68
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-10 15:07:30 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
 	usage: 'ping',
 	execute(client, message, args, db) {
 		message.channel.send('Pong! 🏓').then(sent => {
-			sent.edit(`🏓 Roundtrip latency: ${sent.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms 🏓`);
+			sent.edit(`🏓 Latency is ${Date.now() - message.createdTimestamp}. Roundtrip latency: ${sent.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms 🏓`);
 		});
 	},
 }
