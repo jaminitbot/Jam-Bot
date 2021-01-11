fc47150220467e1a1b8a12e1eb35135daacdf143
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-11 16:22:37 +0000

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -4,7 +4,7 @@ module.exports = {
 	usage: 'Well',
 	execute(client, message, args, db) {
         message.channel.send('The command you found does literally nothing')
-        const nick = message.member.setNickname('Sad Nerd', 'Code')
-        nick.catch(() => {}); // add an empty catch handler
+        message.member.setNickname('Sad Nerd', 'Code')
+            .catch(() => {})
 	},
 };
