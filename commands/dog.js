6d7e19da42468c0474724bfa54d774702c1fcc0f
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-12 22:06:36 +0000

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -5,7 +5,12 @@ module.exports = {
 	usage: 'dog',
 	async execute(client, message, args, db) {
 		if (args[0]){
-			var data = await fetch(`https://dog.ceo/api/breed/${args.join(' ')}/images/random`).then(response => response.json());
+			if (args[1]){
+				var data = await fetch(`https://dog.ceo/api/breed/${args[0]}/${args[1]}/images/random`).then(response => response.json())
+			} else {
+				var data = await fetch(`https://dog.ceo/api/breed/${args[0]}/images/random`).then(response => response.json())
+			}
+			
 		} else {
 			var data = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json())
 		}
