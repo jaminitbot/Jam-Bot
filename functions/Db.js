1a0a442ec6120b07203903dcb699e3ccb417a578
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-13 13:33:26 +0000

    Update Db.js

--- functions/Db.js
+++ functions/Db.js
@@ -13,7 +13,7 @@ module.exports = {
 		})
 		return db
 	},
-	async updateKey(db, guild, key, value) {
+	updateKey(db, guild, key, value) {
 		db.run('UPDATE "' + guild + '" SET \'value\' = \'' + value + '\' WHERE key=\'' + key + '\'', (err) => {
 		  if (err) return console.error(err.message)
 		  db.get('SELECT "value" FROM "' + guild + '" WHERE key="' + key + '"', (err, row) => {
