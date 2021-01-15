abbf3e6d5fb507ef24bfb0e528063d5baf32f81c
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-15 19:16:29 +0000

    No more rates

--- events/message.js
+++ events/message.js
@@ -1,11 +1,5 @@
 const permissions = require('../functions/permission')
 const messages = require('../functions/messages')
-const rateLimit = require('rate-limiter-flexible')
-const opts = {
-	points: 3,
-	duration: 3,
-}
-const rateLimiter = new rateLimit.RateLimiterMemory(opts);
 module.exports = {
 	async register(client, msg, db, config) {
 		if (msg.author.bot) return
@@ -24,21 +18,11 @@ module.exports = {
 				if (!client.commands.has(command)) return // Doesn't have specified command
 				try {
 					if (client.commands.get(command).permissions) {
-						var consumePoints = 3 // Rate limit more for admin stuff
 						if (!permissions.checkperm(msg.member, client.commands.get(command).permissions)) { // User doesn't have specified permissions to run command
 							return msg.channel.send(messages.getPermissionsMessage())
-						} 
-					} else {
-						var consumePoints = 1
+						}
 					}
-					rateLimiter.consume(msg.author.id, consumePoints)
-						.then((rateLimiterRes) => {
-							client.commands.get(command).execute(client, msg, args, db)
-						})
-						.catch((rateLimiterRes) => {
-							msg.reply('Slow down with the commands!')
-						});
-					
+					client.commands.get(command).execute(client, msg, args, db)
 				} catch (error) { // Error running command
 					console.error(error)
 					msg.reply(messages.getErrorMessage())
