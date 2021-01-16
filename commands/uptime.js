6e8d39a99ed4ad39fe3b827543be6d57409c6efb
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-16 17:10:12 +0000

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,18 +1,18 @@
-function msToTime (duration) {
-  const milliseconds = parseInt((duration % 1000) / 100)
-  let seconds = Math.floor((duration / 1000) % 60)
-  let minutes = Math.floor((duration / (1000 * 60)) % 60)
-  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-  	hours = (hours < 10) ? '0' + hours : hours
-  minutes = (minutes < 10) ? '0' + minutes : minutes
-  seconds = (seconds < 10) ? '0' + seconds : seconds
-  return hours + ':' + minutes + ':' + seconds + '.' + milliseconds
+function msToTime(duration) {
+	const milliseconds = parseInt((duration % 1000) / 100)
+	let seconds = Math.floor((duration / 1000) % 60)
+	let minutes = Math.floor((duration / (1000 * 60)) % 60)
+	let hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+	hours = (hours < 10) ? '0' + hours : hours
+	minutes = (minutes < 10) ? '0' + minutes : minutes
+	seconds = (seconds < 10) ? '0' + seconds : seconds
+	return hours + ':' + minutes + ':' + seconds + '.' + milliseconds
 }
 module.exports = {
-  name: 'uptime',
-  description: 'Displays the current uptime!',
-  usage: 'uptime',
-  execute (client, message, args, db) {
-    message.channel.send('The bot has been up for: ' + msToTime(client.uptime))
-  }
+	name: 'uptime',
+	description: 'Displays the current uptime!',
+	usage: 'uptime',
+	execute(client, message, args, db) {
+		message.channel.send('The bot has been up for: ' + msToTime(client.uptime))
+	}
 }
