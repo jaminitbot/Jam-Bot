28cb7fbe1ed35a2e1cb614f73f4645f7a12def2a
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-16 17:24:45 +0000

    Update Db.js

--- functions/Db.js
+++ functions/Db.js
@@ -7,6 +7,7 @@ module.exports = {
 			console.error(err)
 			return null
 		}
+		// Help me
 		const db = new sqlite3.cached.Database(config.settings.databaseLocation, (err) => {
 			if (err) return console.error(err.message)
 			console.log('Connected to the SQlite database')
