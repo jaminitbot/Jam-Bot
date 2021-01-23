5abc2e4e5052f70840dfe2ca36753eb1af04ae82
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-23 22:06:11 +0000

    Update addemoji.js

--- commands/addemoji.js
+++ commands/addemoji.js
@@ -2,11 +2,11 @@ module.exports = {
 	name: 'addemoji',
 	description: 'Purges messages',
 	permissions: '',
-	usage: 'addemoji URL NAME',
+	usage: 'addemoji https://example.com/yes.jpg YESEMOTE',
 	execute(client, message, args, db) {
-		if (!args[0] || !args[1]) return message.reply(this.usage)
+		if (!args[0] || !args[1]) return message.reply('Usage: ' + this.usage)
 		message.guild.emojis.create(args[0], args[1])
-			.then(emoji => console.log(`Created new emoji with name ${emoji.name}!`))
-			.catch(console.error);
+			.then(emoji => message.channel.send(`Created new emoji with name ${emoji.name}!`))
+			.catch(message.channel.send('Error making emoji!'))
 	},
 }
