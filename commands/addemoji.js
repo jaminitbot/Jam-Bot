cfd5f5411bead413e6cd24f32f64eb0f4ef6134c
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-24 09:39:47 +0000

    Update addemoji.js

--- commands/addemoji.js
+++ commands/addemoji.js
@@ -1,15 +1,18 @@
 module.exports = {
 	name: 'addemoji',
-	description: 'Purges messages',
-	permissions: '',
+	description: 'Adds an emoji to the server',
 	usage: 'addemoji EmojiName',
 	execute(client, message, args, db) {
-		if (!args[0]) return message.reply('Make sure you name your emoji')
-		var url = message.attachments.first()
-		console.log(url.url)
-		if (!url) return message.reply('Attach an image')
+		if (!args[0]) return message.reply('you need to specify the name of your emoji!')
+		let url = message.attachments.first()
+		if (!url) return message.reply('you need to attach the image!')
 		message.guild.emojis.create(url.url, args[0])
-			.then(emoji => message.channel.send(`Created new emoji with name ${emoji.name}!`))
+			.then(emoji =>{
+				message.channel.send(`The emoji "${emoji.name}" was created!`).then(sent => {
+					sent.react(emoji.identifier)
+					message.react(emoji.identifier)
+				})
+			})
 			.catch(console.error)
 	},
 }
