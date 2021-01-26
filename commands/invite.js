b92bfc4298490ae9425eb61ff8773aa3ed2b5797
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-26 14:29:55 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Creates an invite for the channel',
 	usage: 'invite',
 	execute(client, message, args, db) {
-		message.channel.createInvite()
+		message.channel.createInvite({maxAge: 0})
   			.then(invite => message.reply('Invite link: ' + invite.url))
   			.catch(console.error)
 	},
