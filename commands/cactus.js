d2636a92031a2b42e1493b8f5cb15cd9bdec6e0e
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-27 20:52:46 +0000

    Create cactus.js

---
+++ commands/cactus.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'cactus',
+	description: 'God knows what this does',
+	permissions: '',
+	usage: 'cactus',
+	execute(client, message, args, db) {
+		message.channel.send('https://cdn.discordapp.com/attachments/797536333064044595/804088627910148136/download_2.jpeg')
+	},
+}
