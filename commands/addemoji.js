c63cbf5fa5c053a941c32834e22a1613145626f1
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-01-28 14:51:50 +0000

    Updates

--- commands/addemoji.js
+++ commands/addemoji.js
@@ -3,9 +3,9 @@ module.exports = {
 	description: 'Adds an emoji to the server',
 	usage: 'addemoji EmojiName',
 	execute(client, message, args, db) {
-		if (!args[0]) return message.reply('you need to specify the name of your emoji!')
+		if (!args[0]) return message.reply('you need to specify a name for your emoji!')
 		let url = message.attachments.first()
-		if (!url) return message.reply('you need to attach the image!')
+		if (!url) return message.reply('you need to attach the image of the emoji!')
 		message.guild.emojis.create(url.url, args[0], {reason: `Uploaded by: ${message.author}, ${message.author.id}`})
 			.then(emoji =>{
 				message.channel.send(`The emoji "${emoji.name}" was created!`).then(sent => {
