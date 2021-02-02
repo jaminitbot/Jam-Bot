f92e86a2077a959f18c8e4dfb37c1f61218b8de6
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-02 13:58:56 +0000

    Update tell.js

--- commands/tell.js
+++ commands/tell.js
@@ -1,8 +1,10 @@
+const messages = require('../functions/messages')
 module.exports = {
 	name: 'tell',
 	description: 'Tells something to someone',
 	usage: '',
 	execute(client, message, args, db) {
+		if (message.author.id == '707313027485270067') return message.channel.send(messages.getPermissionsMessage())
 		const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 		if (!user) return message.reply('you need to mention a valid person!')
 		if (!args[1]) return message.reply('you need to say something to tell them')
