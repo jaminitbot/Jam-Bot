38054a08e4c67daaa3a9e251e4f28221edb006f3
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-02 14:18:57 +0000

    Update tell.js

--- commands/tell.js
+++ commands/tell.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'Tells something to someone',
 	usage: '',
 	execute(client, message, args, db) {
-		if (message.author.id == '707313027485270067') return message.channel.send(messages.getPermissionsMessage())
+		// if (message.author.id == '707313027485270067') return message.channel.send(messages.getPermissionsMessage())
 		const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 		if (!user) return message.reply('you need to mention a valid person!')
 		if (!args[1]) return message.reply('you need to say something to tell them')
