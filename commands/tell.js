3d9b38ed0d59e1061ea2a174f681be68d84991b4
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-02 13:55:18 +0000

    Create tell.js

---
+++ commands/tell.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'tell',
+	description: 'Tells something to someone',
+	usage: '',
+	execute(client, message, args, db) {
+		const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+		if (!user) return message.reply('you need to mention a valid person!')
+		if (args[1]) return message.reply('you need to say something to tell them')
+		message.delete()
+		const thingToSay = args.splice(1).join(' ')
+		message.channel.send(`${thingToSay}, <@${user.id}>`)
+	},
+}
