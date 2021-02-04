a399d2ca3652493f4a316a527cc30b46d7d0121a
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-04 21:30:08 +0000

    Rename code.js to upupdown...

--- commands/upupdowndownleftrightleftrightbastart.js
+++ commands/upupdowndownleftrightleftrightbastart.js
@@ -1,7 +1,6 @@
 module.exports = {
 	name: 'upupdowndownleftrightleftrightbastart',
 	description: 'It\'s a secret',
-	usage: 'Well',
 	execute(client, message, args, db) {
 		message.channel.send('The command you found does literally nothing, you sad nerd.')
 		message.member.setNickname('Sad Nerd', 'upupdowndownleftrightleftrightbastart')
