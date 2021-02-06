917c61b7b20b4fb8f5d98cf33e16a3ac978b0aee
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-06 11:44:22 +0000

    Update pogchamp.js

--- commands/pogchamp.js
+++ commands/pogchamp.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'Gets twitch\'s pogchamp of the day',
 	usage: 'PogChamp',
 	async execute(client, message, args, db) {
-		const data = await fetch('https://raw.githubusercontent.com/MattIPv4/pogchamp/dfff9753a31fa099556d59b391f9020737fa345e/build/data.json').then(response => response.json())
+		const data = await fetch('https://raw.githubusercontent.com/MattIPv4/pogchamp/master/build/data.json').then(response => response.json())
 		message.channel.send(data.img.medium)
 	},
 }
