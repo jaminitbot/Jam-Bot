0dab5994445b90bfd58348994907c071fa4a83d1
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-06 11:48:54 +0000

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -11,12 +11,10 @@ module.exports = {
 		if (!isNumber(args[0])) return message.reply('you need to specify a number!')
 		if (!message.guild.me.hasPermission(['MANAGE_MESSAGES'])) return message.channel.send('I dont have permission to perform this command, make sure I have the manage messages permission!')
 		const deleteCount = parseInt(args[0], 10)
-		if (deleteCount < 1) {
-			message.reply('You can\'t delete less than one message silly!')
-			return
+		if (deleteCount < 1) {	
+			return message.reply('You can\'t delete less than one message silly!')
 		} else if (deleteCount > 99) { // Discord api doesn't let us do more than 100
-			message.reply('You can\'t delete more than 99 messages in one go!')
-			return
+			return message.reply('You can\'t delete more than 99 messages in one go!')
 		}
 		message.channel.bulkDelete(deleteCount + 1).catch(error => {
 			console.error('Error when deleting messages: ' + error)
