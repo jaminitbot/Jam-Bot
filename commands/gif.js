42c5e9f754999d4092423556b2c74bf17c3cdee9
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-06 17:23:41 +0000

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(client, message, args, db) {
 		if (!config.settings.gifyApiKey) return
 		if (!args[0]) return message.reply('You need to specify what to search for!')
-		const sent = await message.channel.send(':mag_right: Finding image...')
+		const sent = await message.channel.send(':mag_right: Finding gif...')
 		const search = args.join(' ')
 		const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${config.settings.gifyApiKey}&q=${search}&rating=pg-13`, {})
 		const json = await response.json()
