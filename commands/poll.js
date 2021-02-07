507cb71b048fa39f5b8f52bef95eb61e26756038
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-07 16:19:20 +0000

    Poll command

---
+++ commands/poll.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: 'poll',
+	description: 'Creates a poll',
+	usage: 'poll Are chips good?',
+	async execute(client, message, args, db) {
+		if (!args[0]) return message.reply('you need to specify what to make the poll about!')
+		const text = args.splice(0).join(' ')
+		let embed = {
+			description: text,
+			footer: {
+				text: `A poll by ${message.author.tag}`,
+				icon_url: message.member.user.avatarURL()
+			},
+			timestamp: Date.now()
+		}
+		const sent = await message.channel.send({embed: embed})
+		sent.react('✅')
+			.then(() => (sent.react('❌')))
+	},
+}
