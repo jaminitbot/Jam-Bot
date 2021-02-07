27467048c4b592ea3de0de72b17f6deb381b529b
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-07 15:55:54 +0000

    Update kickorban.js

--- functions/kickorban.js
+++ functions/kickorban.js
@@ -1,14 +1,14 @@
 module.exports = {
-	execute(message, args, banOrMute) {
-		if (!args[0]) return message.reply('Usage: ' + this.usage)
-		if (!message.guild.me.hasPermission(['BAN_MEMBERS', 'ADMINISTRATOR', 'KICK_MEMBERS'])) return message.channel.send(`I don't have permission to perform this command, check I can ${banOrMute} people!`)
+	execute(message, args, kickOrBan) {
+		if (!args[0]) return message.reply(`usage: ${kickOrBan} @person reason`)
+		if (!message.guild.me.hasPermission(['BAN_MEMBERS', 'ADMINISTRATOR', 'KICK_MEMBERS'])) return message.channel.send(`I don't have permission to perform this command, check I can ${kickOrBan} people!`)
 		const memberToBan = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 		if (!memberToBan) return message.reply('You didn\'t mention a valid user in this server!')
-		if (message.author.id == memberToBan.id) return message.reply(`You can't ${banOrMute} yourself silly!`)
+		if (message.author.id == memberToBan.id) return message.reply(`You can't ${kickOrBan} yourself silly!`)
 		const moderator = message.author.username + '#' + message.author.discriminator
 		const reason = args.splice(1).join(' ')
 		sucessful = true
-		if (banOrMute == 'ban') {
+		if (kickOrBan == 'ban') {
 			memberToBan.ban({ reason: `${moderator}: ${reason}`, days: 1 })
 				.catch(sucessful = false)
 		} else {
@@ -16,7 +16,7 @@ module.exports = {
 				.catch(sucessful = false)
 		}
 		if (sucessful) {
-			message.reply(`${memberToBan} was ${banOrMute}ed with reason: ${reason}`)
+			message.reply(`${memberToBan} was ${kickOrBan}ed with reason: ${reason}`)
 		} else {
 			message.channel.send('It may have worked, it may have not')
 		}
