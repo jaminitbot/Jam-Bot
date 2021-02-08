5801d6be44ce45e7c5c42263ac7d5129b58ef8ca
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-08 10:45:35 +0000

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -4,6 +4,7 @@ module.exports = {
 	usage: 'poll Are chips good?',
 	async execute(client, message, args, db) {
 		if (!args[0]) return message.reply('you need to specify what to make the poll about!')
+		message.delete()
 		const text = args.splice(0).join(' ')
 		let embed = {
 			description: text,
