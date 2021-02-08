edea927665087c22d3c08b853ba4a0fc9275e3e2
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-08 20:27:12 +0000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ module.exports = {
 	async register(member){
 		if (member.guild.id == '779060204528074783'){
 			let aRole
-			if (member.id == '438815630884601856'){
+			if (member.id == '438815630884601856' || member.id == '265933873358045185' || member.id == '724842710221193217' || member.id == '312272690473992202'){
 				aRole = await member.guild.roles.cache.find(role => role.id == "781999680832929813")
 				member.roles.add(aRole)
 				aRole = await member.guild.roles.cache.find(role => role.id == "789810162696192011")
