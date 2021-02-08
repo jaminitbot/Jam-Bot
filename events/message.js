2f63029a9e6d5a02d8292d8eaa8f3473aaddf210
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-08 20:47:56 +0000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -8,7 +8,7 @@ module.exports = {
 		if (message.author.bot) return
 		if (bannedIds.includes(message.author.id)) return
 		if (String(message.channel.name).includes('juan') && !(String(message.content) == 'juan')) message.delete()
-		if (message.author.id == '523963702245064725') {
+		if (message.author.id == '523963702245064725' || message.author.id == '582305080200396811') {
 			if (random.int(min=1, max=10) == 5){
 				message.delete()
 			}
