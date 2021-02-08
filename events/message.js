2024abd6c27e5822e6e5f2d42ea4130193895c50
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-08 20:48:29 +0000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -8,11 +8,6 @@ module.exports = {
 		if (message.author.bot) return
 		if (bannedIds.includes(message.author.id)) return
 		if (String(message.channel.name).includes('juan') && !(String(message.content) == 'juan')) message.delete()
-		if (message.author.id == '523963702245064725' || message.author.id == '582305080200396811') {
-			if (random.int(min=1, max=10) == 5){
-				message.delete()
-			}
-		}
 		const guild = message.guild
 		db.get('SELECT "value" FROM "' + guild + '" WHERE key="prefix"', (err, row) => { // Get prefix
 			if (err) return console.log(err)
