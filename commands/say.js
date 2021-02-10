6f97afbeced78f0d10004f58981440670b42a745
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-10 12:40:50 +0000

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,14 @@
+const config = require('../config.json')
+module.exports = {
+	name: 'say',
+	description: 'Say',
+	usage: 'say #general Hiiii',
+	execute(client, message, args, db, logger) {
+		if (!message.author.id == config.settings.ownerid) return
+		message.delete()
+		const channel = message.guild.channels.cache.get(args[0])
+		if (!channel) return message.reply('you need to specify a valid channel')
+		const thingToSay = args.splice(1).join(' ')
+		channel.send(thingToSay)
+	},
+}
