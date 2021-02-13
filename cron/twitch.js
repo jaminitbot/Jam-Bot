38d10435f4fc560c72e42e0dd22e23b53e47cae2
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-13 16:24:47 +0000

    Overhaul of twitch notifications

--- cron/twitch.js
+++ cron/twitch.js
@@ -1,9 +1,11 @@
 const fetch = require('node-fetch')
 const database = require('../functions/db')
 const messages = require('../functions/messages')
+const md5 = require('md5')
 module.exports = {
 	async execute(client, db, config) {
-		const response = await fetch('https://api.twitch.tv/helix/search/channels?query=honkserini', {
+		if (!config.settings.twitchApiClientId || !config.settings.twitchApiSecret) return
+		const response = await fetch('https://api.twitch.tv/helix/search/channels?query=' + config.settings.twitchNotificationsUsername, {
 			headers: {
 				'CLIENT-ID': config.settings.twitchApiClientId,
 				'Authorization': 'Bearer ' + config.settings.twitchApiSecret
@@ -12,29 +14,26 @@ module.exports = {
 		const json = await response.json()
 		const data = json.data[0]
 		if (data.is_live) {
-			db.get('SELECT "value" FROM "' + '779060204528074783' + '" WHERE key="LiveTime"', (err, row) => {
-				if (err) return
-				const notificationChannel = client.channels.cache.get('780071463473643550')
-				if (!notificationChannel) return
-				if (row) if (row.value == data.started_at) {
-					db.get('SELECT "value" FROM "' + '779060204528074783' + '" WHERE key="LiveTitle"', (err, row) => {
-						if (!row) return
-						if (data.title == row.value) {
-							return
-						} else {
-							db.get('SELECT "value" FROM "' + '779060204528074783' + '" WHERE key="LiveMessageId"', (err, row) => {
-								if (!row) return
-								const messageToUpdate = await notificationChannel.messages.fetch(row.value)
-								messageToUpdate.edit(`${messages.getHappyMessage()} @everyone ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/honkserini>`)
-							})
-						}
-					})
-					
+			let LiveTime = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTime"')
+			const notificationChannel = client.channels.cache.get(config.settings.twitchNotificationsChannel)
+			if (!notificationChannel) return
+			if (LiveTime.value == data.started_at) {
+				let LiveTitle = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTitle"')
+				if (!LiveTitle) return
+				if (md5(data.title) == LiveTitle.value) {
+					return
+				} else {
+					database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTitle', md5(data.title))
+					let MessageId = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveMessageId"')
+					if (!MessageId) return
+					let messageToUpdate = await notificationChannel.messages.fetch(MessageId.value)
+					messageToUpdate.edit(`${messages.getHappyMessage()} @everyone ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/${data.broadcaster_login}>`)
+					return
 				}
-				database.updateKey(db, '779060204528074783', 'LiveTime', data.started_at)
-				const sentMessage = await notificationChannel.send(`${messages.getHappyMessage()} @everyone ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/honkserini>`)
-				database.updateKey(db, '779060204528074783', 'LiveMessageId', sentMessage.id)
-			})
+			}
+			database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTime', data.started_at)
+			const sentMessage = await notificationChannel.send(`${messages.getHappyMessage()} @everyone ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/${data.broadcaster_login}>`)
+			database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveMessageId', sentMessage.id)
 		}
 	}
 }
--- functions/db.js
+++ functions/db.js
@@ -15,17 +15,28 @@ module.exports = {
 		return db
 	},
 	updateKey(db, guild, key, value) {
+		console.log(`Updating ${key} with value: ${value}`)
 		db.run('UPDATE "' + guild + '" SET \'value\' = \'' + value + '\' WHERE key=\'' + key + '\'', (err) => {
-			if (err) return logger.error(err.message)
+			if (err) return console.error('Error updating: ' + err.message)
 			db.get('SELECT "value" FROM "' + guild + '" WHERE key="' + key + '"', (err, row) => {
-				if (err) return logger.error(err.message)
+				if (err) return console.error(err.message)
 				if (!row) {
 					db.run('INSERT INTO "' + guild + '" (key, value) VALUES (\'' + key + '\', \'' + value + '\')', (err) => {
-						if (err) return logger.error(err.message)
+						if (err) return console.error(err.message)
 					})
 				}
 			})
-			if (err) return logger.error(err.message)
+			if (err) return console.error(err.message)
 		})
+	},
+	get(db, query, params) {
+		return new Promise(function(resolve, reject) {
+			db.get(query, params, function(err, row)  {
+				if(err) reject("Read error: " + err.message)
+				else {
+					resolve(row)
+				}
+			})
+		}) 
 	}
 }
