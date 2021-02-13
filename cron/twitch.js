fe8f2920f364a3c94c87ef2242e108cfd0c0fdb4
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-13 15:24:13 +0000

    Update twitch.js

--- cron/twitch.js
+++ cron/twitch.js
@@ -16,9 +16,24 @@ module.exports = {
 				if (err) return
 				const notificationChannel = client.channels.cache.get('780071463473643550')
 				if (!notificationChannel) return
-				if (row) if (row.value == data.started_at) return // Stops us notifying more than once for one live
+				if (row) if (row.value == data.started_at) {
+					db.get('SELECT "value" FROM "' + '779060204528074783' + '" WHERE key="LiveTitle"', (err, row) => {
+						if (!row) return
+						if (data.title == row.value) {
+							return
+						} else {
+							db.get('SELECT "value" FROM "' + '779060204528074783' + '" WHERE key="LiveMessageId"', (err, row) => {
+								if (!row) return
+								const messageToUpdate = await notificationChannel.messages.fetch(row.value)
+								messageToUpdate.edit(`${messages.getHappyMessage()} @everyone ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/honkserini>`)
+							})
+						}
+					})
+					
+				}
 				database.updateKey(db, '779060204528074783', 'LiveTime', data.started_at)
-				notificationChannel.send(`${messages.getHappyMessage()} @everyone ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/honkserini>`)
+				const sentMessage = await notificationChannel.send(`${messages.getHappyMessage()} @everyone ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/honkserini>`)
+				database.updateKey(db, '779060204528074783', 'LiveMessageId', sentMessage.id)
 			})
 		}
 	}
