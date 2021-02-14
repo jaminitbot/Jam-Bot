eb7e0f0b971a0ca38d6d2220e6bd0e2a9eaf3e6a
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-02-14 21:18:32 +0000

    Create removemodrole.js

---
+++ commands/removemodrole.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'removemodrole',
+	description: 'Remove mod role',
+	permissions: '',
+	usage: '',
+	execute(client, message, args, db, logger) {
+		const user = message.guild.members.cache.get(args[0])
+		user.roles.remove('796109738793631804')
+		user.roles.remove('808444712145911838')
+	},
+}
