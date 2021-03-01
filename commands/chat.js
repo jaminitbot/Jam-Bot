b71bd3a4dc09be5fda6c9c33c28549be25bdb2a1
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-01 14:08:57 +0000

    Better

--- commands/chat.js
+++ commands/chat.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'chat',
 	description: 'Chats with the bot',
 	usage: 'chat how are you',
-	execute(client, message, args, db, logger) {
+	async execute(client, message, args, db, logger) {
 		const userMessage = args.splice(0).join(' ')
 		const { response } = await fetch('https://some-random-api.ml/chatbot?message=' + encodeURIComponent(userMessage)).then(response => response.json())
 		message.channal.send(response)
