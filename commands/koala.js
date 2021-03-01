9e34421cc9f0c1d2243369740b6a043131045c68
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-01 14:14:26 +0000

    OMG

---
+++ commands/koala.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch')
+module.exports = {
+	name: 'koala',
+	description: 'Koala',
+	usage: 'koala',
+	async execute(client, message, args, db, logger) {
+		const { file } = await fetch('https://some-random-api.ml/img/koala').then(response => response.json())
+		message.channel.send(link || 'Unable to get a koala, the api\'s probably down')
+	}
+}
