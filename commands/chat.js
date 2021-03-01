c09a5aeee12e93750bffab44c8b9a9268b3edccb
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-01 14:09:48 +0000

    Update chat.js

--- commands/chat.js
+++ commands/chat.js
@@ -6,6 +6,6 @@ module.exports = {
 	async execute(client, message, args, db, logger) {
 		const userMessage = args.splice(0).join(' ')
 		const { response } = await fetch('https://some-random-api.ml/chatbot?message=' + encodeURIComponent(userMessage)).then(response => response.json())
-		message.channal.send(response)
+		message.channel.send(response)
 	},
 }
