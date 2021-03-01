3c32afd40184c3a4c1fca9707f12504e97da250c
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-01 14:08:14 +0000

    Better

--- commands/chat.js
+++ commands/chat.js
@@ -4,8 +4,8 @@ module.exports = {
 	description: 'Chats with the bot',
 	usage: 'chat how are you',
 	execute(client, message, args, db, logger) {
-		const message = args.splice(0).join(' ')
-		const { response } = await fetch('https://some-random-api.ml/chatbot?message=' + encodeURIComponent(message)).then(response => response.json())
+		const userMessage = args.splice(0).join(' ')
+		const { response } = await fetch('https://some-random-api.ml/chatbot?message=' + encodeURIComponent(userMessage)).then(response => response.json())
 		message.channal.send(response)
 	},
 }
