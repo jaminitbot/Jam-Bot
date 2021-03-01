e7b6a3ff89f522d1a2a9f546c7f3c1e1ca2ff668
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-01 14:03:23 +0000

    Birb

--- commands/bird.js
+++ commands/bird.js
@@ -5,6 +5,10 @@ module.exports = {
 	usage: 'bird',
 	async execute(client, message, args, db, logger) {
 		const { link } = await fetch('https://some-random-api.ml/img/birb').then(response => response.json())
+		const { fact } = await fetch('https://some-random-api.ml/facts/bird').then(response => response.json())
+		if (fact){
+			message.channel.send(fact)
+		}
 		message.channel.send(link || 'Unable to get a birb, the api\'s probably down')
 	}
 }
