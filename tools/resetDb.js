c200e8fb298363692c1d6a992ba72033d835227e
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-02 20:09:42 +0000

    Update resetDb.js

--- tools/resetDb.js
+++ tools/resetDb.js
@@ -1,4 +1,22 @@
 const config = require('../config.json')
 const dbScript = require('../functions/db')
-const db = dbScript.connect(config)
-dbScript.updateKey(db, '779060204528074783', 'LiveTime', '0')
+const { createLogger, format, transports } = require('winston');
+const winston = require('winston')
+const { combine, timestamp, label, printf } = format;
+const loggingFormat = printf(({ level, message, label, timestamp }) => {
+	return `${timestamp} ${level}: ${message}`;
+  });
+const logger = winston.createLogger({
+	level: 'info',
+	format: combine(
+		timestamp(),
+		loggingFormat
+	  ),
+	transports: [
+		new winston.transports.Console(),
+		new winston.transports.File({ filename: 'error.log', level: 'error' }),
+		new winston.transports.File({ filename: 'combined.log' }),
+	],
+})
+const db = dbScript.connect(config, logger)
+dbScript.updateKey(db, '779060204528074783', 'LiveTime', 'NONE')
