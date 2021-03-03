5e844551fba3ba7c5f516c51d1301da73f78ce5f
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-03 13:56:30 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,10 +1,11 @@
 const config = require('../config.json')
+const messages = require('../functions/messages')
 module.exports = {
 	name: 'eval',
 	description: 'Executes code',
 	usage: 'eval 1+1',
 	execute(client, message, args, db, logger) {
-		if (!message.author.id == config.settings.ownerid) return
+		if (!(message.author.id == config.settings.ownerid)) return message.channel.send(messages.getPermissionsMessage())
 		message.channel.send(String(eval(args.splice(0).join(' '))))
 	},
 }
