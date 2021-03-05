a94dc1fcc82bb5ae2d361087884f2189311cf877
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-05 20:14:14 +0000

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,9 +1,10 @@
+const moment = require('moment');
 module.exports = {
 	name: 'uptime',
 	description: 'Displays the bot\'s current uptime',
 	usage: 'uptime',
 	execute(client, message, args, db, logger) {
-		date = new Date(client.uptime);
-		message.channel.send('The bot has been up since: ' + date.toString())
+		let TimeDate = new Date(Date.now() - client.uptime)
+		message.channel.send('The bot has been up since: ' + TimeDate.toString())
 	}
 }
