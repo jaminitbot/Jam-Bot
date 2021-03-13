caff9a18d764ad50654e3ef95380bbc57db98a03
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-13 18:10:51 +0000

    Clean up a bit

--- commands/owner.js
+++ commands/owner.js
@@ -4,10 +4,6 @@ module.exports = {
 	description: 'Displays the owner of the bot',
 	usage: 'owner',
 	async execute(client, message, args, db, logger) {
-		message.react('🇯')
-			.then(() => (message.react('🇦')))
-			.then(() => (message.react('🇲')))
-			.then(() => (message.react('🇪')))
-			.then(() => (message.react('🇸')))
+		message.channel.send('jamesatjaminit#6406')
 	}
 }
--- commands/upupdowndownleftrightleftrightbastart.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'upupdowndownleftrightleftrightbastart',
-	description: 'It\'s a secret',
-	execute(client, message, args, db, logger) {
-		message.channel.send('The command you found does literally nothing, you sad nerd.')
-		message.member.setNickname('Sad Nerd', 'upupdowndownleftrightleftrightbastart')
-			.catch(() => { })
-	}
-}
