107818fb2d63f42c2dd36acb63ae3822a81d15db
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-13 11:35:37 +0000

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,6 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'basics', label: 'Docs'},
-    {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
