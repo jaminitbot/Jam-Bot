5cf5402303a6e4eb1a60ec8b7ba3cb47f23bfb7f
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-14 20:29:26 +0000

    Fix

--- cron/twitch.js
+++ cron/twitch.js
@@ -1,46 +1,46 @@
-const fetch = require('node-fetch')
-const database = require('../functions/db')
-const messages = require('../functions/messages')
-const sha1 = require('sha1')
-const message = require('../events/message')
-module.exports = {
-	async execute(client, db, config, logger) {
-		if (!config.settings.twitchApiClientId || !config.settings.twitchApiSecret) return
-		if (!config.settings.twitchNotificationsChannel || !config.settings.twitchNotificationsGuild) return
-		const response = await fetch('https://api.twitch.tv/helix/search/channels?query=' + config.settings.twitchNotificationsUsername, {
-			headers: {
-				'CLIENT-ID': config.settings.twitchApiClientId,
-				'Authorization': 'Bearer ' + config.settings.twitchApiSecret
-			}
-		})
-		const json = await response.json()
-		const data = json.data[0]
-		if (data.is_live) { // Checks if broadcaster is live
-			const notificationChannel = client.channels.cache.get(config.settings.twitchNotificationsChannel)
-			let notificationMessageContent = `<@&814796307402457148> ${messages.getHappyMessage()} ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/${data.broadcaster_login}>`
-			if (!notificationChannel) return
-			let LiveTime = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTime"')
-			if (LiveTime == data.started_at) { // Checks if we have already notified for this live
-				let LiveTitle = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTitle"')
-				if (!LiveTitle) {
-					database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTitle', sha1(data.title))
-				}
-				// NOTE: hash because we don't want the title to contain SQL escaping characters
-				if (sha1(data.title) == LiveTitle) { // If the title in the message and title of stream is the same, do nothing
-					return 
-				} else { // If not
-					database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTitle', sha1(data.title)) // Put the new title in the db
-					let MessageId = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveMessageId"') // Get the message id of the notiication we sent
-					if (MessageId) {
-						let messageToUpdate = await notificationChannel.messages.fetch(MessageId) // Get the message object
-						await messageToUpdate.edit(notificationMessageContent) // Edit the notification message with the new title
-					}
-				}
-			} else { // We haven't notified for this live
-				database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTime', data.started_at) // Put the time of live in db so we don't notify twice
-				const sentMessage = await notificationChannel.send(notificationMessageContent) // Notify for the live in the right channel
-				database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveMessageId', sentMessage.id) // Put the notification message id in db so we can edit the message later
-			}
-		}
-	}
-}
+// const fetch = require('node-fetch')
+// const database = require('../functions/db')
+// const messages = require('../functions/messages')
+// const sha1 = require('sha1')
+// const message = require('../events/message')
+// module.exports = {
+// 	async execute(client, db, config, logger) {
+// 		if (!config.settings.twitchApiClientId || !config.settings.twitchApiSecret) return
+// 		if (!config.settings.twitchNotificationsChannel || !config.settings.twitchNotificationsGuild) return
+// 		const response = await fetch('https://api.twitch.tv/helix/search/channels?query=' + config.settings.twitchNotificationsUsername, {
+// 			headers: {
+// 				'CLIENT-ID': config.settings.twitchApiClientId,
+// 				'Authorization': 'Bearer ' + config.settings.twitchApiSecret
+// 			}
+// 		})
+// 		const json = await response.json()
+// 		const data = json.data[0]
+// 		if (data.is_live) { // Checks if broadcaster is live
+// 			const notificationChannel = client.channels.cache.get(config.settings.twitchNotificationsChannel)
+// 			let notificationMessageContent = `<@&814796307402457148> ${messages.getHappyMessage()} ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/${data.broadcaster_login}>`
+// 			if (!notificationChannel) return
+// 			let LiveTime = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTime"')
+// 			if (LiveTime == data.started_at) { // Checks if we have already notified for this live
+// 				let LiveTitle = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTitle"')
+// 				if (!LiveTitle) {
+// 					database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTitle', sha1(data.title))
+// 				}
+// 				// NOTE: hash because we don't want the title to contain SQL escaping characters
+// 				if (sha1(data.title) == LiveTitle) { // If the title in the message and title of stream is the same, do nothing
+// 					return 
+// 				} else { // If not
+// 					database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTitle', sha1(data.title)) // Put the new title in the db
+// 					let MessageId = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveMessageId"') // Get the message id of the notiication we sent
+// 					if (MessageId) {
+// 						let messageToUpdate = await notificationChannel.messages.fetch(MessageId) // Get the message object
+// 						await messageToUpdate.edit(notificationMessageContent) // Edit the notification message with the new title
+// 					}
+// 				}
+// 			} else { // We haven't notified for this live
+// 				database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTime', data.started_at) // Put the time of live in db so we don't notify twice
+// 				const sentMessage = await notificationChannel.send(notificationMessageContent) // Notify for the live in the right channel
+// 				database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveMessageId', sentMessage.id) // Put the notification message id in db so we can edit the message later
+// 			}
+// 		}
+// 	}
+// }
