a2a2176517c320ee03a89e013850815d45f95269
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-14 20:23:00 +0000

    Fix

--- cron/twitch.js
+++ cron/twitch.js
@@ -20,19 +20,19 @@ module.exports = {
 			let notificationMessageContent = `<@&814796307402457148> ${messages.getHappyMessage()} ${data.display_name} is live streaming: ${data.title}\n<https://www.twitch.tv/${data.broadcaster_login}>`
 			if (!notificationChannel) return
 			let LiveTime = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTime"')
-			if (LiveTime.value == data.started_at) { // Checks if we have already notified for this live
+			if (LiveTime == data.started_at) { // Checks if we have already notified for this live
 				let LiveTitle = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveTitle"')
 				if (!LiveTitle) {
 					database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTitle', sha1(data.title))
 				}
 				// NOTE: hash because we don't want the title to contain SQL escaping characters
-				if (sha1(data.title) == LiveTitle.value) { // If the title in the message and title of stream is the same, do nothing
+				if (sha1(data.title) == LiveTitle) { // If the title in the message and title of stream is the same, do nothing
 					return 
 				} else { // If not
 					database.updateKey(db, config.settings.twitchNotificationsGuild, 'LiveTitle', sha1(data.title)) // Put the new title in the db
 					let MessageId = await database.get(db, 'SELECT "value" FROM "' + config.settings.twitchNotificationsGuild + '" WHERE key="LiveMessageId"') // Get the message id of the notiication we sent
 					if (MessageId) {
-						let messageToUpdate = await notificationChannel.messages.fetch(MessageId.value) // Get the message object
+						let messageToUpdate = await notificationChannel.messages.fetch(MessageId) // Get the message object
 						await messageToUpdate.edit(notificationMessageContent) // Edit the notification message with the new title
 					}
 				}
