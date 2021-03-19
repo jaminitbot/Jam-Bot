d7d01c3d0475cf9ad0bf2fdfb01ed4bf6f8de8d3
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-19 22:11:21 +0000

    yes

--- functions/db.js
+++ functions/db.js
@@ -8,7 +8,7 @@ module.exports = {
 			logger.error(err)
 			return null
 		}
-		const db = new Keyv(process.env.DATABASE_URL || config.settings.databaseLocation)
+		const db = new Keyv(process.env.DATABASE_URL)
 		return db
 	},
 	async updateKey(db, guild, key, value) {
