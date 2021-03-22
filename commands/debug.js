4714cc26931573ae0dfe1e69e202f9736627666d
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-22 21:24:19 +0000

    Debug command

---
+++ commands/debug.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'debug',
+	description: 'Displays debug information',
+	permissions: ['ADMINISTRATOR'],
+	usage: 'debug',
+	async execute(client, message, args, db, logger) {
+		const sent = await message.channel.send('Loading...')
+		// sent.edit(`🏓 Roundtrip latency: ${sent.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms 🏓`) // https://discordjs.guide/popular-topics/faq.html#how-to-check-the-bots-ping
+		let TimeDate = new Date(Date.now() - client.uptime)
+		let embed = {
+			title: 'Debug Information',
+			description: `Roundtrip: ${sent.createdTimestamp - message.createdTimestamp}ms\nAPI: ${Math.round(client.ws.ping)}ms\nRevision: ${process.env.GIT_REV}\nUptime: ${TimeDate.toString()}`
+		}
+		sent.edit({embed:embed})
+	},
+}
