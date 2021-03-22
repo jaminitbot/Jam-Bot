16ace3e697e83d06e6cf322ca94e4e6f2a3d0d77
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-22 21:27:02 +0000

    More debug

--- commands/debug.js
+++ commands/debug.js
@@ -9,7 +9,7 @@ module.exports = {
 		let TimeDate = new Date(Date.now() - client.uptime)
 		let embed = {
 			title: 'Debug Information',
-			description: `Roundtrip: ${sent.createdTimestamp - message.createdTimestamp}ms\nAPI: ${Math.round(client.ws.ping)}ms\nRevision: ${process.env.GIT_REV}\nUptime: ${TimeDate.toString()}`
+			description: `Roundtrip: ${sent.createdTimestamp - message.createdTimestamp}ms\nAPI: ${Math.round(client.ws.ping)}ms\nRevision: ${process.env.GIT_REV}\nUptime: ${TimeDate.toString()}\nReady: ${client.readyAt}`
 		}
 		sent.edit({embed:embed})
 	},
