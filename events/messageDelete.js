3b8f3d24af922c3ae4b244fbe332a888c92855a9
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-23 17:29:10 +0000

    Don't modlog owner

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,7 +3,7 @@ const config = require('../config.json')
 module.exports = {
 	async register(client, message, db) {
 		if (message.author.bot) return
-		// if (message.author.id == config.settings.ownerid) return
+		if (message.author.id == config.settings.ownerid) return
 		let logDeletes = await db.get(message.guild, 'logDeletedMessages')
 		if (logDeletes) {
 			let modLogChannnel = await db.get(message.guild, 'modLogChannel')
