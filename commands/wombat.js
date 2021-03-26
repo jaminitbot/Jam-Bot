9a7cebc1b7bac84f936b3b0e8332a4153f58c95f
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-26 18:01:00 +0000

    Wombat

--- commands/wombat.js
+++ commands/wombat.js
@@ -1,11 +1,11 @@
 const image = require('./image')
 const random = require('random')
 module.exports = {
-	name: 'forg',
-	description: 'Frog',
-	usage: 'frog',
+	name: 'wombat',
+	description: 'Wombat',
+	usage: 'Wombat',
 	execute(client, message, args, db, logger) {
-		let tempArgs = [random.int(min=1, max=25), 'frog'] // eslint-disable-line no-undef
+		let tempArgs = [random.int(min=1, max=25), 'wombat'] // eslint-disable-line no-undef
 		image.execute(client, message, tempArgs, db)
 	},
 }
