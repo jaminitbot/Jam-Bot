baccca0be6482658a0a8039ea7f90fad79cd2bb7
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-03-26 18:30:53 +0000

    Update debug.js

--- commands/debug.js
+++ commands/debug.js
@@ -5,11 +5,10 @@ module.exports = {
 	usage: 'debug',
 	async execute(client, message, args, db, logger) {
 		const sent = await message.channel.send('Loading...')
-		// sent.edit(`🏓 Roundtrip latency: ${sent.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms 🏓`) // https://discordjs.guide/popular-topics/faq.html#how-to-check-the-bots-ping
 		let TimeDate = new Date(Date.now() - client.uptime)
 		let embed = {
 			title: 'Debug Information',
-			description: `Roundtrip: ${sent.createdTimestamp - message.createdTimestamp}ms\nAPI: ${Math.round(client.ws.ping)}ms\nRevision: ${process.env.GIT_REV}\nUptime: ${TimeDate.toString()}`,
+			description: `Roundtrip: ${sent.createdTimestamp - message.createdTimestamp}ms\nAPI: ${client.ws.ping}ms\nRevision: ${process.env.GIT_REV}\nUp since: ${TimeDate.toString()}`,
 			footer: {
 				text: `Intiated by ${message.author.tag}`,
 				icon_url: message.author.displayAvatarURL()
