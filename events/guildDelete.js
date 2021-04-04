ee6a7065e9477f1e8725bfef2e45bbfcb6622690
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-04-04 21:34:24 +0100

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,9 +1,5 @@
 module.exports = {
 	register(guild, db) {
-		// db.run('DROP TABLE "' + guild.id + '"', (err) => { // Delete the table for the guild
-		// 	if (err) {
-		// 		return logger.error(err.message)
-		// 	}
-		// })
+		guild.client.channels.cache.get(process.env.GuildLogChannel).send(`Oh dear, we left ${guild.name}, ${guild.id}`)
 	}
 }
