f9b2ce6cc99607947150ca471d8ce06316e034b0
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-04-05 19:25:03 +0000

    oh no

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,16 +1,16 @@
 const ytdl = require('ytdl-core')
 module.exports = {
 	async register(client, oldState, newState) {
-		if (oldState.member.user.bot) return
-		if (!newState.channelID) return
-		if (!(newState.member.id == '724842710221193217'|| newState.member.id == )
-		let voiceChannel = client.channels.cache.get(newState.channelID)
-		if (!voiceChannel) return
-		voiceChannel.join().then(connection => {
-			const stream = ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', { filter: 'audioonly' });
-			const dispatcher = connection.play(stream);
+		// if (oldState.member.user.bot) return
+		// if (!newState.channelID) return
+		// if (!(newState.member.id == '724842710221193217'|| newState.member.id == )
+		// let voiceChannel = client.channels.cache.get(newState.channelID)
+		// if (!voiceChannel) return
+		// voiceChannel.join().then(connection => {
+		// 	const stream = ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ', { filter: 'audioonly' });
+		// 	const dispatcher = connection.play(stream);
 			
-			dispatcher.on('finish', () => voiceChannel.leave());
-		})
+		// 	dispatcher.on('finish', () => voiceChannel.leave());
+		// })
 	}
 }
