3d3567d0cf9077e1e4463c71dc173331a9c88d04
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-04-29 20:51:28 +0100

    Yarn v2 (#36)

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        'no-unused-vars': 0,
+        'no-undef': 0,
+    },
+}
