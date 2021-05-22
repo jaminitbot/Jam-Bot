24b45deba0f432a96d6e339f279287aab0ee23d0
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-05-22 21:17:47 +0100

    Add guild id to debug info

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -11,9 +11,11 @@ export async function execute(client: client, message: Message, args, logger: Lo
 	let TimeDate = new Date(Date.now() - client.uptime)
 	let embed = {
 		title: 'Debug Information',
-		description: `Roundtrip: \`${sent.createdTimestamp - message.createdTimestamp
-			}ms\`\nAPI: \`${client.ws.ping}ms\`\nRevision: \`${process.env.GIT_REV || 'N/A'
-			}\`\nUp since: \`${TimeDate.toString()}\``,
+		description: `Roundtrip: \`${sent.createdTimestamp - message.createdTimestamp}ms\`\n
+		API: \`${client.ws.ping}ms\`\n
+		Revision: \`${process.env.GIT_REV || 'N/A'}\`\n
+		Up since: \`${TimeDate.toString()}\`\n
+		Guild ID: \`${message.guild.id}\``,
 		footer: {
 			text: `Intiated by ${message.author.tag}`,
 			icon_url: message.author.displayAvatarURL(),
