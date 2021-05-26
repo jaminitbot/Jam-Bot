9a6ab1f60618c8466bbd616ee9389e4669adf599
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-05-26 20:24:09 +0100

    Allow entering a guild in setkey and getkey

--- src/commands/getkey.ts
+++ src/commands/getkey.ts
@@ -5,11 +5,10 @@ import { getKey } from '../functions/db'
 
 export const name = 'getkey'
 export const description = 'Gets a db key'
-export const usage = 'getkey blah'
+export const usage = 'getkey 4569844357398443 blah'
 export async function execute(client: client, message: Message, args, logger: Logger) {
-	if (!(message.author.id == process.env.OWNERID)) return
-	if (!args[0]) return message.reply('You need to specify a key to get')
-	message.channel.send(
-		(await getKey(message.guild.id, args[0])) || 'null'
-	)
+	if (message.author.id !== process.env.OWNERID) return
+	if (!args[0]) return message.reply('You need to specify a guild ID')
+	if (!args[1]) return message.reply('You need to specify a key to get')
+	message.channel.send((await getKey(args[0], args[1])) || 'null')
 }
--- src/commands/setkey.ts
+++ src/commands/setkey.ts
@@ -7,10 +7,13 @@ export const name = 'setkey'
 export const description = 'Sets a db key'
 export const usage = 'setkey blah test'
 export async function execute(client: client, message: Message, args, logger: Logger) {
-	if (!(message.author.id == process.env.OWNERID)) return
-	const result: boolean = await setKey(message.guild.id, args[0], args[1])
+	if (message.author.id !== process.env.OWNERID) return
+	if (!args[0]) return message.reply('You need to specify a guild ID')
+	if (!args[1]) return message.reply('You need to specify a key to set')
+	if (!args[2]) return message.reply('You need to specify a value to set')
+	const result: boolean = await setKey(args[0], args[1], args[2])
 	if (result) {
-		message.channel.send(`Successfully set ${args[0]} to ${args[1]}`)
+		message.channel.send(`Successfully set ${args[1]} to ${args[2]} in guild: ${args[0]}`)
 	} else {
 		message.channel.send('Oops, that didn\'t work!')
 	}
