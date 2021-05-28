38dd1d5414cb7f52c6f9410df9c7918344f512a3
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-05-28 17:08:23 +0100

    Improve getkey and setkey logic

--- src/commands/getkey.ts
+++ src/commands/getkey.ts
@@ -1,14 +1,33 @@
-import { Message } from "discord.js"
+import { Message, MessageEmbed } from "discord.js"
 import { client } from '../customDefinitions'
 import { Logger } from "winston"
 import { getKey } from '../functions/db'
+const isNumber = require('is-number')
 
 export const name = 'getkey'
 export const description = 'Gets a db key'
 export const usage = 'getkey 4569844357398443 blah'
 export async function execute(client: client, message: Message, args, logger: Logger) {
 	if (message.author.id !== process.env.OWNERID) return
-	if (!args[0]) return message.reply('You need to specify a guild ID')
-	if (!args[1]) return message.reply('You need to specify a key to get')
-	message.channel.send((await getKey(args[0], args[1])) || 'null')
+	let guild
+	let key
+	if (!isNumber(args[0]) && !args[1]) { // Getting key without guild input
+		console.log('Yes')
+		guild = message.guild.id // Use current guild
+		key = args[0]
+	} else if (isNumber(args[0]) && !args[1]) { // Guild ID inputted but no key to get
+		return message.reply('You need to input a value to set!')
+	} else { // Using guild id
+		guild = args[0]
+		key = args[1]
+	}
+	const valueReturned = String(await getKey(guild, key))
+	const embed = new MessageEmbed
+	embed.setTitle('GetKey')
+	embed.addField('Guild', guild, true)
+	embed.addField('Key', key, true)
+	embed.addField('Value', valueReturned, true)
+	embed.setFooter(`Intiated by ${message.author.tag}`, message.author.displayAvatarURL())
+	embed.setTimestamp(Date.now())
+	message.channel.send(embed)
 }
--- src/commands/setkey.ts
+++ src/commands/setkey.ts
@@ -1,19 +1,39 @@
-import { Message } from "discord.js"
+import { Message, MessageEmbed } from "discord.js"
 import { client } from '../customDefinitions'
 import { Logger } from "winston"
 import { setKey } from '../functions/db'
+const isNumber = require('is-number')
 
 export const name = 'setkey'
 export const description = 'Sets a db key'
 export const usage = 'setkey 46435456789132 blah test'
 export async function execute(client: client, message: Message, args, logger: Logger) {
 	if (message.author.id !== process.env.OWNERID) return
-	if (!args[0]) return message.reply('You need to specify a guild ID')
-	if (!args[1]) return message.reply('You need to specify a key to set')
-	if (!args[2]) return message.reply('You need to specify a value to set')
-	const result: boolean = await setKey(args[0], args[1], args[2])
+	let guild
+	let key
+	let value
+	if (!isNumber(args[0]) && !args[2]) { // Setting key without guild input
+		guild = message.guild.id // Use current guild
+		key = args[0]
+		value = args[1]
+	} else if (isNumber(args[0]) && !args[2]) { // Guild ID inputted but no value to set
+		return message.reply('You need to input a value to set!')
+	} else { // Using guild id
+		guild = args[0]
+		key = args[1]
+		value = args[2]
+	}
+	const result: boolean = await setKey(guild, key, value)
 	if (result) {
-		message.channel.send(`Successfully set ${args[1]} to ${args[2]} in guild: ${args[0]}`)
+		const embed = new MessageEmbed
+		embed.setTitle('SetKet')
+		embed.setDescription('Successfully set key')
+		embed.addField('Guild', guild, true)
+		embed.addField('Key', key, true)
+		embed.addField('Value', value, true)
+		embed.setFooter(`Intiated by ${message.author.tag}`, message.author.displayAvatarURL())
+		embed.setTimestamp(Date.now())
+		message.channel.send(embed)
 	} else {
 		message.channel.send('Oops, that didn\'t work!')
 	}
