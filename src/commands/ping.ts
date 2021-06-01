ea39812583fe754f32677f782836e2369a8f3407
James Cook <james@jaminit.co.uk>
2021-06-01 21:53:26 +0100

    Update ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -7,8 +7,9 @@ export const description = 'Displays various latency information'
 export const usage = 'ping'
 export async function execute(client: client, message: Message, args, logger: Logger) {
 	message.react('🏓')
-	const sent = await message.channel.send('Pong! 🏓')
+	const sent = await message.channel.send('Pinging...')
 	const embed = new MessageEmbed
-	embed.setDescription(`:hourglass: ${sent.createdTimestamp - message.createdTimestamp}ms :stopwatch: ${Math.round(client.ws.ping)}ms`)
+	embed.setDescription(`:stopwatch: ${sent.createdTimestamp - message.createdTimestamp}ms :hourglass: ${Math.round(client.ws.ping)}ms`)
+	embed.setFooter('Roundtrip and api latency')
 	sent.edit({ content: null, embed: embed })
 }
