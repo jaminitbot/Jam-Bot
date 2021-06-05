f5ec39f957ca48ed31d7a3ea869b50ebfdc7173f
James Cook <james@jaminit.co.uk>
2021-06-05 10:46:40 +0100

    Improve and fix logging

--- src/index.ts
+++ src/index.ts
@@ -30,27 +30,28 @@ const clientOptions = {
 // @ts-expect-error
 const client: client = new Client(clientOptions)
 
-const {createLogger, format, transports} = require('winston')
-const {combine, timestamp, printf} = format
+import {createLogger, transports, format} from "winston";
+
 
     // eslint-disable-next-line no-unexpected-multiline
 (async function () {
     // Logging
-    const loggingFormat = printf(({level, message, timestamp}) => {
-        return `${timestamp} ${level}: ${message}`
-    })
     const logger = createLogger({
         level: 'info',
-        format: combine(timestamp(), loggingFormat),
+        format: format.json(),
         transports: [
             new transports.File({
                 filename: 'error.log',
                 level: 'error',
             }),
-            new transports.Console(),
             new transports.File({filename: 'combined.log'}),
         ],
     })
+    if (process.env.NODE_ENV !== 'production') {
+        logger.add(new transports.Console({
+            format: format.simple(),
+        }));
+    }
     // Registers all the commands in the commands folder
     // https://discordjs.guide/command-handling/dynamic-commands.html#how-it-works
     client.commands = new Collection
