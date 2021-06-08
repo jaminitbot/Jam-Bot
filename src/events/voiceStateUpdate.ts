7d815f102d708c1ff6e35ffd26a11863d47ff726
James Cook <james@jaminit.co.uk>
2021-06-08 18:37:06 +0100

    Timeout

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,12 +1,13 @@
 import { VoiceState } from "discord.js";
 import {randomInt} from '../functions/util'
+import {snipeLifetime} from "../functions/snipe";
 export default function register(oldState: VoiceState, newState: VoiceState) {
 	if (newState.member.user.bot) return
 	if (newState.channelID) {
 		if (newState.member.id == '261222439512440832' || newState.member.id == '707313027485270067') {
 			if (randomInt(1, 5) == 3) {
 				try {
-					newState.kick('One in 5 chance of being disconnected, sadly you got disconnected')
+					setTimeout(() => newState.kick('One in 5 chance of being disconnected, sadly you got disconnected'), randomInt(1,10) * 1000)
 				} catch {
 					// eslint-disable-next-line no-empty
 					{}
