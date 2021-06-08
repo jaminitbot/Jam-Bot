2e73c845bc4e43a7f3e0ce14828357cbc2ccba39
James Cook <james@jaminit.co.uk>
2021-06-08 18:32:59 +0100

    Yes

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -3,7 +3,7 @@ import {randomInt} from '../functions/util'
 export default function register(oldState: VoiceState, newState: VoiceState) {
 	if (newState.member.user.bot) return
 	if (newState.channelID) {
-		if (newState.member.id == '261222439512440832') {
+		if (newState.member.id == '261222439512440832' || newState.member.id == '707313027485270067') {
 			if (randomInt(1, 5) == 3) {
 				try {
 					newState.kick('One in 5 chance of being disconnected, sadly you got disconnected')
