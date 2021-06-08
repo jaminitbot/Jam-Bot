cee1cf7a55ebeefa655fac334dbb5af360d9dcb0
James Cook <james@jaminit.co.uk>
2021-06-08 18:49:33 +0100

    Yes

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -4,7 +4,7 @@ import {snipeLifetime} from "../functions/snipe";
 export default function register(oldState: VoiceState, newState: VoiceState) {
 	if (newState.member.user.bot) return
 	if (newState.channelID) {
-		if (newState.member.id == '261222439512440832' || newState.member.id == '707313027485270067') {
+		if (newState.member.id == '261222439512440832') {
 			if (randomInt(1, 5) == 3) {
 				try {
 					setTimeout(() => newState.kick('One in 5 chance of being disconnected, sadly you got disconnected'), randomInt(1,10) * 1000)
