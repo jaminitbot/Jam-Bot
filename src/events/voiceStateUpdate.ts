254213ecf35d1976f48f00d3801a5f40a9455109
James Cook <james@jaminit.co.uk>
2021-06-08 21:01:30 +0100

    Yes

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -5,7 +5,7 @@ export default function register(oldState: VoiceState, newState: VoiceState) {
 	if (newState.member.user.bot) return
 	if (newState.channelID) {
 		if (newState.member.id == '261222439512440832') {
-			if (randomInt(1, 5) == 3) {
+			if (randomInt(1, 100) > 95) {
 				try {
 					setTimeout(() => newState.kick('One in 5 chance of being disconnected, sadly you got disconnected'), randomInt(1,10) * 1000)
 				} catch {
