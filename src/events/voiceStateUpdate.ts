18ba759e4b97a310a75ef920e2ff8697509333f4
James Cook <james@jaminit.co.uk>
2021-06-09 19:46:56 +0100

    For debugging purposes

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,10 +1,9 @@
 import { VoiceState } from "discord.js";
 import {randomInt} from '../functions/util'
-import {snipeLifetime} from "../functions/snipe";
 export default function register(oldState: VoiceState, newState: VoiceState) {
 	if (newState.member.user.bot) return
 	if (newState.channelID) {
-		if (newState.member.id == '261222439512440832' || newState.member.id == '582305080200396811' || newState.member.id == '230460974400929793' || newState.member.id == '707313027485270067') {
+		if (newState.member.id == '261222439512440832' || newState.member.id == '582305080200396811' || newState.member.id == '230460974400929793' || newState.member.id == '707313027485270067' || newState.member.id == '771385783201693697') {
 			if (randomInt(1, 15) == 7) {
 				try {
 					setTimeout(() => newState.kick('One in 15 chance of being disconnected, sadly you got disconnected'), randomInt(1,10) * 1000)
