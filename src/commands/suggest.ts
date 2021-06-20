0b4745dbe001741c91be7eee4bc66740cb9062e8
James Cook <james@jaminit.co.uk>
2021-06-20 16:36:15 +0100

    No more docker

--- src/commands/suggest.ts
+++ src/commands/suggest.ts
@@ -12,35 +12,35 @@ export const usage = 'suggest Make a memes channel'
 export async function execute(client: client, message: Message, args, logger: Logger) {
 	if (!args[0])
 		return message.reply('You need to specify what to suggest!')
-	const suggestionChannel = await getKey(
+	const suggestionChannelId = await getKey(
 		message.guild.id,
 		'suggestionChannel'
 	)
-	if (!suggestionChannel)
+	if (!suggestionChannelId)
 		return message.channel.send(
 			"Looks like suggestions haven't been setup here yet!"
 		)
 	// @ts-expect-error
-	const channel: TextChannel = await client.channels.fetch(suggestionChannel)
-	if (!channel) return message.channel.send('Error finding suggestions channel, perhaps it\'s being deleted')
-	const suggestion = args.splice(0).join(' ')
+	const suggestionChannel: TextChannel = await client.channels.fetch(suggestionChannelId)
+	if (!suggestionChannel) return message.channel.send('Error finding suggestions channel, perhaps it\'s being deleted')
+	const suggestionDescription = args.join(' ')
 	message.delete()
 	let suggestionCount = await getKey(message.guild.id, 'suggestionCount')
 	if (!suggestionCount) suggestionCount = 0
 	suggestionCount = parseInt(suggestionCount)
-	setKey(message.guild.id, 'suggestionCount', suggestionCount + 1)
+	await setKey(message.guild.id, 'suggestionCount', suggestionCount + 1)
 	const embed = new MessageEmbed
 	embed.setTitle(`Suggestion #${suggestionCount + 1}`)
-	embed.addField('Description', suggestion)
+	embed.addField('Description', suggestionDescription)
 	if (message.attachments.first()) {
 		embed.setImage(message.attachments.first().url)
 	}
 	embed.setColor('#E9D985')
 	embed.setFooter('Suggestion by ' + message.author.tag, message.author.displayAvatarURL())
 	embed.setTimestamp(Date.now())
-	const suggestmessage = await channel.send(embed)
-	message.reply('Suggestion logged!')
-	await suggestmessage.react('✅')
+	const suggestionMessage = await suggestionChannel.send(embed)
+	await message.reply('Suggestion logged!')
+	await suggestionMessage.react('✅')
 	await delay(1050)
-	await suggestmessage.react('❌')
+	await suggestionMessage.react('❌')
 }
