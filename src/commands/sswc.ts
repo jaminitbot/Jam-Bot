285f275c9f21ca780b2b0f3ad16dbd83ec00a600
James Cook <james@jaminit.co.uk>
2021-06-25 16:14:08 +0100

    More rick roll links

--- src/commands/sswc.ts
+++ src/commands/sswc.ts
@@ -17,7 +17,17 @@ const rickRollCodes = [
     'vkbQmH5MPME',
     '8O_ifyIIrN4',
     'ikFZLI4HLpQ',
-    '0SoNH07Slj0'
+    '0SoNH07Slj0',
+    'xfr64zoBTAQ',
+    'cqF6M25kqq4',
+    'j5a0jTc9S10',
+    'dPmZqsQNzGA',
+    'ID_L0aGI9bg',
+    'REWyCy_m39Q',
+    'nHRbZW097Uk',
+    'kKi-AKvWHGM',
+    'm6OCJxOsiyw',
+    'z6wNtevjVOk'
 ]
 export async function execute(client: client, message: Message, args, logger: Logger) {
     message.channel.send(`<https://youtu.be/${rickRollCodes[randomInt(0, rickRollCodes.length -1)]}>`)
