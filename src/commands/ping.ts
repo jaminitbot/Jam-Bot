9ebd341ccd1d111d687cbe18a3f2f89200f15be6
James Cook <james@jaminit.co.uk>
2021-07-07 18:31:01 +0100

    Fix ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -9,8 +9,8 @@ export async function execute(client: client, message: Message, args) {
 	const sent = await message.channel.send('Pinging...')
 	const embed = new MessageEmbed
 	const ping = Date.now() - message.createdTimestamp
-	embed.setDescription(`:stopwatch: ${sent.createdTimestamp - message.createdTimestamp}ms :hourglass: ${Math.round(client.ws.ping)}ms :alarm_clock: ${message.createdTimestamp}`)
-	embed.setFooter('Roundtrip, api latency and ping')
+	embed.setDescription(`:stopwatch: ${sent.createdTimestamp - message.createdTimestamp}ms :hourglass: ${Math.round(client.ws.ping)}ms`)
+	embed.setFooter('Roundtrip and api latency')
 	embed.setColor('#FB21CB')
 	await sent.edit({ content: null, embed: embed })
 	message.react('🏓')
