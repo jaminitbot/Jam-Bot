8ce8ede2ee3f0a70c008c5f4c83d4400fe5a3618
James Cook <james@jaminit.co.uk>
2021-07-10 21:29:24 +0100

    Prevent sending secrets in eval command

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,7 +1,14 @@
 import { Message, MessageEmbed } from "discord.js"
 import { client } from '../customDefinitions'
 import { getInvalidPermissionsMessage } from '../functions/messages'
-
+const secrets = [
+	process.env.token,
+	process.env.MONGO_URL,
+	process.env.twitchApiSecret,
+	process.env.twitchApiClientId,
+	process.env.pexelsApiKey
+].filter(Boolean)
+const secretsRegex = RegExp(secrets.join('|'), 'g')
 export const name = 'eval'
 export const description = 'Executes code'
 export const usage = 'eval 1+1'
@@ -16,6 +23,13 @@ export async function execute(client: client, message: Message, args) {
 			commandOutput = await eval(command)
 		} catch (error) {
 			commandOutput = error
+		}
+		commandOutput = String(commandOutput)
+			.replace(secretsRegex, '[secret]')
+		commandOutput = String(commandOutput)
+		if (commandOutput.length > 1024) {
+			commandOutput = commandOutput.substring(0, commandOutput.length - (commandOutput.length - 1024))
+			embed.addField('NOTE', 'Output was truncated to send via discord')
 		}
 		embed.addField('Output', commandOutput)
 		message.channel.send(embed)
