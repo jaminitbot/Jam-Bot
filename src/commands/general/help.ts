a6f6f5d0f505b5af5803105ebec6ce58ee2454fe
James Cook <james@jaminit.co.uk>
2021-07-11 14:53:50 +0100

    Update help command with new information and new command folders

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -12,13 +12,14 @@ export async function execute(client: client, message: Message, args) {
 		const commandToFind = String(args[0]).toLowerCase()
 		if (commandToFind && !(commandToFind == ' ')) {
 			const prefix = await getKey(message.guild.id, 'prefix') || process.env.DEFAULTPREFIX
-			if (client.commands.has(commandToFind)) {
-				embed.setTitle('Help: ' + prefix + commandToFind)
-				const command = client.commands.get(commandToFind)
+			const command = client.commands.get(commandToFind) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandToFind));
+			if (command) {
+				embed.setTitle('Help: ' + prefix + command.name)
 				const description = command.description ?? 'None'
 				const usage = command.usage ? prefix + command.usage : prefix + commandToFind
 				embed.addField('Description', description, true)
 				embed.addField('Usage', usage, true)
+				if (command.aliases) embed.addField('Aliases', command.aliases.toString(), true)
 				if (command.permissions) {
 					const permissionsNeeded = command.permissions.toString()
 					embed.addField('Permissions Needed', permissionsNeeded, true)
@@ -30,7 +31,7 @@ export async function execute(client: client, message: Message, args) {
 	} else {
 		// Generic help command
 		embed.setTitle('Help')
-		embed.setDescription('You can view a list of commands [here](https://jambot.jaminit.co.uk/docs/)')
+		embed.setDescription('You can view a list of commands [here](https://jambot.jaminit.co.uk/)')
 	}
 	embed.setColor('#439A86')
 	message.channel.send(embed)
