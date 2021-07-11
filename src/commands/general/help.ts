f8b4a43398f6b0a1dc7b38a84632b6435ff6c1a8
James Cook <james@jaminit.co.uk>
2021-07-11 15:06:44 +0100

    Update docs url

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -31,7 +31,7 @@ export async function execute(client: client, message: Message, args) {
 	} else {
 		// Generic help command
 		embed.setTitle('Help')
-		embed.setDescription('You can view a list of commands [here](https://jambot.jaminit.co.uk/)')
+		embed.setDescription('You can view a list of commands [here](https://jambot.jaminit.co.uk/#/commands/basicCommands)')
 	}
 	embed.setColor('#439A86')
 	message.channel.send(embed)
