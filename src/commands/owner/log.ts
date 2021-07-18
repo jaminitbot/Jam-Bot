cb1b14d8beeec28fab57460cfdc30664f3e24a39
James Cook <james@jaminit.co.uk>
2021-07-18 19:09:46 +0100

    Fix invalid perms message for log.ts

--- src/commands/owner/log.ts
+++ src/commands/owner/log.ts
@@ -2,13 +2,13 @@ import {Message} from "discord.js"
 import {client} from '../../customDefinitions'
 const fs = require('fs')
 import {uploadToHasteBin} from '../../functions/util'
-
+import {returnInvalidPermissionMessage} from '../../functions/util'
 export const name = 'log'
 export const description = 'Uploads the log for easy viewing'
 export const usage = 'log error|combined'
 export const aliases = ['uploadlog']
 export async function execute(client: client, message: Message, args) {
-    if (message.author.id != process.env.OWNERID) return message.channel.send('Haha no')
+    if (message.author.id != process.env.OWNERID) return returnInvalidPermissionMessage(message)
     let logFilePath = args[0] ? args[0].toString().toLowerCase() : null
     if (!logFilePath) {
         logFilePath = 'combined.log'
