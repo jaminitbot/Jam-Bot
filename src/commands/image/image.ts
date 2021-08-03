8907d7e4d7bc16ca6f1fb4fc061bc408766024bf
James Cook <james@jaminit.co.uk>
2021-08-03 11:19:44 +0100

    feat: add image slash command

--- src/commands/image/image.ts
+++ src/commands/image/image.ts
@@ -1,4 +1,4 @@
-import {Message} from "discord.js"
+import {CommandInteraction, Message} from "discord.js"
 import {client} from '../../customDefinitions'
 import gis = require('g-i-s');
 import isImage = require('is-image');
@@ -17,15 +17,41 @@ export const slashCommandOptions = [{
 },
     {
         name: 'position',
-        type: 'STRING',
+        type: 'INTEGER',
         description: '(optional) the specific position of image to get',
         required: false
     }]
-
+async function searchForImage(search, position, nsfw, imageType):Promise<string> {
+    return new Promise(function(resolve, reject) {
+        let searchOptions = {
+            searchTerm: search
+        }
+        // @ts-ignore
+        if (!nsfw) {
+            // Nsfw channels can bypass safe search
+            // @ts-expect-error
+            searchOptions.queryStringAddition = '&safe=active' // Enable safe search, better than nothing, filters most things
+        }
+        const validImageUrls = []
+        gis(searchOptions, function (error, results) {
+            results.forEach((element) => {
+                if (isImage(element.url)) {
+                    validImageUrls.push(element.url)
+                }
+            })
+            if (position) {
+                // Get specific image at position
+                resolve(validImageUrls[position] || `There isn't an ${imageType} for position: ${position}`)
+            } else {
+                resolve(validImageUrls[0] || `No ${imageType} found for your search.`)
+            }
+        })
+    });
+}
 export async function execute(client: client, message: Message, args) {
-    if (!args[0])
-        return message.reply('you need to specify what to search for!')
+    if (!args[0]) return message.reply('you need to specify what to search for!')
     let splitBy = 0
+    let position
     if (isNumber(args[0])) {
         // User wants to get a specific result
         if (args[0] < 1) {
@@ -34,6 +60,7 @@ export async function execute(client: client, message: Message, args) {
             )
         }
         splitBy = 1 // Make sure we don't include the position in the search
+        position = args[0]
     }
     let imageType: string
     //#region Janky Gif Code
@@ -44,37 +71,19 @@ export async function execute(client: client, message: Message, args) {
         imageType = 'image'
     }
     //#endregion
-    const searchOptions = {}
-    // @ts-ignore
-    if (!message.channel.nsfw) {
-        // Nsfw channels can bypass safe search
-        // @ts-expect-error
-        searchOptions.queryStringAddition = '&safe=active' // Enable safe search, better than nothing, filters most things
-    }
     const sentMessage = await message.channel.send(`:mag_right: Finding ${imageType}...`)
+    const search = args.splice(splitBy).join(' ')
     // @ts-expect-error
-    searchOptions.searchTerm = args.splice(splitBy).join(' ')
-    const validImageUrls = []
-    gis(searchOptions, function (error, results) {
-        if (error)
-            return sentMessage.edit(
-                'An error occurred while getting your results :c'
-            )
-        results.forEach((element) => {
-            if (isImage(element.url)) {
-                validImageUrls.push(element.url)
-            }
-        })
-        if (splitBy == 1) {
-            // Get specific image at position
-            sentMessage.edit(
-                validImageUrls[args[0] - 1] ||
-                `There isn't an ${imageType} for position: ` + args[0]
-            )
-        } else {
-            sentMessage.edit(
-                validImageUrls[0] || `No ${imageType} found for your search.`
-            )
-        }
-    })
+    const isNsfw = message.channel.nsfw
+    const imageUrl = await searchForImage(search, position,isNsfw , imageType)
+    sentMessage.edit(imageUrl)
+}
+export async function executeSlash(client, interaction:CommandInteraction) {
+    interaction.defer()
+    const search = interaction.options.getString('search')
+    const position = interaction.options.getInteger('position')
+    // @ts-expect-error
+    const isNsfw = interaction.channel.nsfw
+    const imageUrl = await searchForImage(search, position, isNsfw, 'image')
+    interaction.editReply({content: imageUrl})
 }
