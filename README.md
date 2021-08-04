77cddc042191f59bbc27c7a13eb57167fb4c387d
James Cook <james@jaminit.co.uk>
2021-08-04 17:27:40 +0100

    feat: register owner slash commands to dev server

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ A discord bot designed to be easy to setup, but full of features.
 
 1. Clone the repo `git clone https://github.com/jamesatjaminit/Jam-Bot`
 2. `cd Jam-Bot`
-3. Install dependencies with `yarn install` or `npm i`
+3. Install dependencies with `yarn install`
 4. Set the environment variables outlined in `.env.example`. Either copy to `built\.env` or in production set them as env variables
 5. Compile typescript `yarn tsc`
-6. `npm run start`
+6. `yarn start`
 
 ## TODO:
 
--- src/functions/registerSlashCommands.ts
+++ src/functions/registerSlashCommands.ts
@@ -3,6 +3,7 @@ export default async function register(client:client) {
     client.logger.verbose('Registering slash commands...')
     const commands = client.commands
     let data = []
+    let devData = []
     commands.forEach(command => {
         if (!command.exposeSlash) {
             data[data.length] = {
@@ -10,7 +11,14 @@ export default async function register(client:client) {
                 description: command.description,
             }
             if (command.slashCommandOptions) data[data.length - 1]['options'] = command.slashCommandOptions
+        } else {
+            devData[devData.length] = {
+                name: command.name,
+                description: command.description,
+            }
+            if (command.slashCommandOptions) devData[devData.length - 1]['options'] = command.slashCommandOptions
         }
     })
-    await client.application?.commands.set(data);
+    await client.application?.commands.set(data)
+    if (process.env.devServerId) await client.guilds.cache.get(process.env.devServerId)?.commands.set(devData)
 }
