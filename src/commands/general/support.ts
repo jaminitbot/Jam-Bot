3209094deadc6affebc39b1aac3932e196416ce6
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-09-18 23:03:22 +0100

    feat: support command

---
+++ src/commands/general/support.ts
@@ -0,0 +1,39 @@
+import { MessageButton, Message, MessageEmbed, MessageActionRow, CommandInteraction } from "discord.js"
+import { BotClient } from '../../customDefinitions'
+import { SlashCommandBuilder } from '@discordjs/builders'
+
+export const name = 'support'
+export const description = 'Shows various support information'
+export const usage = 'support'
+export const allowInDm = false
+export const slashData = new SlashCommandBuilder()
+	.setName(name)
+	.setDescription(description)
+
+async function returnSupportEmbed() {
+	const embed = new MessageEmbed
+	embed.setTitle('Support information')
+	embed.setDescription('Click the links below to see the command docs or join our support server!')
+	const row = new MessageActionRow
+	row.addComponents(
+		new MessageButton()
+			.setStyle('LINK')
+			.setLabel('Command Docs')
+			.setURL('https://jambot.jaminit.co.uk/#/'),
+		new MessageButton()
+			.setStyle('LINK')
+			.setLabel('Support Server')
+			.setURL('https://discord.gg/XpNeCGhTfx')
+	)
+	return [embed, row]
+}
+export async function execute(client: BotClient, message: Message, args: Array<unknown>) {
+	const embedObject = await returnSupportEmbed()
+	// @ts-expect-error
+	message.channel.send({ embeds: [embedObject[0]], components: [embedObject[1]] })
+}
+
+export async function executeSlash(client: BotClient, interaction: CommandInteraction) {
+	// @ts-expect-error
+	interaction.reply({ embeds: [embedObject[0]], components: embedObject[1] })
+}
