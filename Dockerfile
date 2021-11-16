710f0c13ac0882b19533ea5a30f1fb601e68a91a
James Cook <47297250+jamesatjaminit@users.noreply.github.com>
2021-11-16 20:36:22 +0000

    fix(metrics): expose metrics on 0.0.0.0

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM node:lts
 WORKDIR /app
 COPY . /app
 RUN curl -f https://get.pnpm.io/v6.16.js | node - add --global pnpm
-RUN pnpm install\
-	&& pnpm install -g typescript
+RUN NODE_ENV=production pnpm install && pnpm install -g typescript
 RUN tsc
 CMD pnpm start
--- src/functions/metrics.ts
+++ src/functions/metrics.ts
@@ -28,7 +28,7 @@ export async function initProm() {
             return 'An error occurred'
         }
     })
-    server.listen(process.env.PROM_PORT ?? 3000)
+    server.listen(process.env.PROM_PORT ?? 3000, '0.0.0.0')
 }
 
 type InteractionType = 'command' | 'context_menu' | 'autocomplete' | 'button' | 'select_menu'
